e45837eee8934bb3d37eef6d3b58457f535cf93e
poitrek <22658450+poitrek@users.noreply.github.com>
2024-01-04 14:28:44 +0100

    105 faster solution

--- Problems/105_construct_binary_tree_from_preorder_and_inorder.cpp
+++ Problems/105_construct_binary_tree_from_preorder_and_inorder.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <stack>
 #include <vector>
 
 using namespace std;
@@ -21,7 +22,7 @@ struct TreeNode {
 /// that belong to the left subtree, and those that belong to the right subtree.
 /// Algorithm outline: take first element of preorder, find it in inorder, partition the sequence into elements before
 /// and after the root in inorder, repeat the process recursively for both sequences (subtrees).
-/// Time complexity: average ~O(n logn), pessimistic O(n^2). Likely can be done faster.
+/// Time complexity: average ~O(n logn), pessimistic O(n^2). Is done faster in Solution3
 class Solution1 {
 	TreeNode* buildTreeRec(vector<int>& preorder, int pre_start, vector<int>& inorder, int in_start, int in_end) {
 		/// empty subtree
@@ -77,7 +78,56 @@ public:
     }
 };
 
-int main() {
+/// @brief Third solution. O(n) time complexity, no recursion. Use stack to store nodes for which we are
+/// currently traversing their left (first) subtree. When encountering a value in preorder, we enter its
+/// left subtree. Once we find that value in inorder, we enter its right subtree. However, we know that
+/// the next value to appear in inorder will be on top of the stack, because it is the most recent one we
+/// pushed on the stack. Thus, if the value in inorder we currently point at equals the top of the stack,
+/// go to its right (second) subtree and pop it from the stack. We then repeat this procedure until we 
+/// reach the end of inorder sequence.
+class Solution3 {
+	enum class ChildDirection {
+		Left, Right
+	};
+	static constexpr int GUARD_VALUE = -3001;
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		auto preorder_it = preorder.begin();
+		auto inorder_it = inorder.begin();
+		stack<TreeNode*> nstack;
+		/// root
+		TreeNode* n = new TreeNode(*preorder_it);
+		TreeNode* root = n;
+		ChildDirection nextChildDir = ChildDirection::Left;
+		nstack.push(new TreeNode(GUARD_VALUE)); /// guard
+		nstack.push(n);
+		while (inorder_it != inorder.end()) {
+			if (*inorder_it == nstack.top()->val) {
+				n = nstack.top();
+				nstack.pop();
+				inorder_it++;
+				nextChildDir = ChildDirection::Right;
+			}
+			else {
+				int v = *(++preorder_it);
+				if (nextChildDir == ChildDirection::Left) {
+					n->left = new TreeNode(v);
+					n = n->left;
+				}
+				else {
+					n->right = new TreeNode(v);
+					n = n->right;
+				}
+				nstack.push(n);
+				nextChildDir = ChildDirection::Left;
+			}
+		}
+		return root;
+    }
 
+};
+
+
+int main() {
 	return 0;
 }
